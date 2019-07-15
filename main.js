5887973f13a3d51c1352f6df02392f9f2ad2e88e
YouRun <kls6378@naver.com>
2019-07-15 23:05:10 +0900

    Start express

--- main.js
+++ main.js
@@ -1,46 +1,53 @@
-var http = require('http');
-var url = require('url');
-var topic = require('./lib/topic');
-var author = require('./lib/author')
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>res.send('Hello World!'))
+
+app.listen(3000,()=>console.log('Example app listening on port 3000!'))
+
+// var http = require('http');
+// var url = require('url');
+// var topic = require('./lib/topic');
+// var author = require('./lib/author')
 
 
-var app = http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+// var app = http.createServer(function (request, response) {
+//     var _url = request.url;
+//     var queryData = url.parse(_url, true).query;
+//     var pathname = url.parse(_url, true).pathname;
 
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            topic.home(request,response);
-        } else {
-            topic.page(request,response);
-        }
-    } else if (pathname === '/create') {
-        if (queryData.id === undefined) {
-            topic.create(request,response);
-        }
-    } else if (pathname === '/create_process') {
-        topic.create_process(request,response);
-    } else if (pathname === '/update') {
-        topic.update(request,response);
-    } else if (pathname === '/update_process') {
-        topic.update_process(request,response);
-    } else if (pathname === '/delete_process') {
-        topic.delete_process(request,response);
-    } else if(pathname === '/author'){
-        author.home(request,response);
-    } else if(pathname === '/author/create_process'){
-        author.create_process(request,response);
-    }else if(pathname === '/author/update'){
-        author.update(request,response);
-    }else if(pathname === '/author/update_process'){
-        author.update_process(request,response);
-    }else if(pathname === '/author/delete_process'){
-        author.delete_process(request,response);
-    } else {
-        response.writeHead(404);
-        response.end('Not found');
-    }
+//     if (pathname === '/') {
+//         if (queryData.id === undefined) {
+//             topic.home(request,response);
+//         } else {
+//             topic.page(request,response);
+//         }
+//     } else if (pathname === '/create') {
+//         if (queryData.id === undefined) {
+//             topic.create(request,response);
+//         }
+//     } else if (pathname === '/create_process') {
+//         topic.create_process(request,response);
+//     } else if (pathname === '/update') {
+//         topic.update(request,response);
+//     } else if (pathname === '/update_process') {
+//         topic.update_process(request,response);
+//     } else if (pathname === '/delete_process') {
+//         topic.delete_process(request,response);
+//     } else if(pathname === '/author'){
+//         author.home(request,response);
+//     } else if(pathname === '/author/create_process'){
+//         author.create_process(request,response);
+//     }else if(pathname === '/author/update'){
+//         author.update(request,response);
+//     }else if(pathname === '/author/update_process'){
+//         author.update_process(request,response);
+//     }else if(pathname === '/author/delete_process'){
+//         author.delete_process(request,response);
+//     } else {
+//         response.writeHead(404);
+//         response.end('Not found');
+//     }
 
-});
-app.listen(3000);
+// });
+// app.listen(3000);
